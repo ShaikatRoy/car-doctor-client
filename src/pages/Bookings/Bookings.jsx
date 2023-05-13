17b6bae3a9622cd270af8cb723494f1f3aff153d
Shaikat roy <shaikatroy258@gmail.com>
2023-05-14 03:06:59 +0600

    services sec updated

---
+++ src/pages/Bookings/Bookings.jsx
@@ -0,0 +1,99 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
+import BookingsRow from "./BookingsRow";
+import Swal from 'sweetalert2'
+
+const Bookings = () => {
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
+
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, []);
+
+       
+   const handleDelete = id => {
+    const proceed = confirm ('Are you dure you want to delete?')
+    if(proceed){
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                Swal.fire(
+                    'opps!',
+                    'Service deleted successfully!',
+                    'success'
+                  )
+                  const remaining = bookings.filter(booking => booking._id !== id);
+                    setBookings(remaining);
+            }
+        })
+    }
+   }
+
+   const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({status: 'confirm'})
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            if(data.modifiedCount > 0){
+                // update state
+                const remaining = bookings.filter(booking => booking._id !== id);
+                const updated = bookings.find(booking => booking._id === id);
+                updated.status = 'confirm';
+                const newBookings = [updated, ...remaining];
+                setBookings(newBookings);
+           
+            }
+          })  
+   }
+
+    return (
+        <div>
+            <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                            bookings.map(booking => <BookingsRow
+                                key={booking._id}
+                                booking={booking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
+                                ></BookingsRow>)
+                       }
+                     </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Bookings;
